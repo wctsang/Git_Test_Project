8fbad3ceacada17487c37c90aae0ef790a41630f
Alex Tsang <alextsangxli@hotmail.com>
2023-10-20 12:09:33 +0900

    PullRequestするために変更

--- Git_Test_Project.cpp
+++ Git_Test_Project.cpp
@@ -7,7 +7,7 @@ int main()
 {
     std::cout << "Hello VisualStudio Git!\n";
 
-    std::cout << "Hello New Branch!\n";
+    std::cout << "PullRequest branch changed!\n";
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
