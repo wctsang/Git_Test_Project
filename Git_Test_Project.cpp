463a6f9cc190911e6bcc05f4336c5ea006725011
wctsang <147121595+wctsang@users.noreply.github.com>
2023-11-17 12:23:33 +0900

    リモートリポジトリでコミット変更

--- Git_Test_Project.cpp
+++ Git_Test_Project.cpp
@@ -7,9 +7,7 @@ int main()
 {
     std::cout << "Hello VisualStudio Git!\n";
 
-    std::cout << "PullRequest branch changed!\n";
-
-    std::cout << "third branch\n";
+    std::cout << "GitHub Clone Changed!\n";
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
